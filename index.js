984961c453d9330d289454cddea336b362e5ba67
StalkerDimous <david.strazovsky@gmail.com>
2019-11-28 12:05:43 +0100

    Adding tzpewrite and editing root

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 
-//Question root
+//Const Root
 const rl = require('readline-sync');
 const chalk = require('chalk');
+const t = require('node-typewriter');
 const log = console.log;
+const pause = rl.question
+const quest = rl.question
+
+/* Program Prescripts
+1. System Message: log(chalk.yellow(''))
+
+
+*/
+t('Hello World!').then(function(){
+    console.log('Typing finished');
+});
+//variables
+        //characters
+            let postava1 = 'Mystery Man';
+            let menoh = 'a'
+
+//welcome
+log(chalk.greenBright('Database Entered'))
+log(chalk.yellow('Insert Name'))
+menoh = quest();
+``
+log(chalk.yellow('Hello person. This is a welcoming message.'))
+log(chalk.yellow('>>PRESS ENTER TO CONTINUE<<'))
+
+
 
 //Rules
 log(chalk.yellow('\n>Here are few rules and mechanics how to play this game:<\n >1.Move to the next step by pressing ENTER<'))
-rl.question()
+pause()
 log(chalk.yellow('2.Great Job! Now this applies to every chat action.'))
-log(chalk.yellow(''))
-
-
-//characters
-let postava1 = 'Mystery Man';
+pause()
+log(chalk.yellow('3'))
+pause()
 
 //Name Ask
 log(chalk.cyan(postava1 + ': '));
 log(chalk.cyan('Hello traveler, what is your name?\n'))
-let menoh = rl.question();
 log(`Nice to meet you ${menoh}`);
 
-
-
-rl.question()
-
+pause()
 
 //Mystery man speaking
 log(`\n${postava1}: `)
